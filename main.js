14e371b20f6d55523d8fd14325c877d3e5efd656
devmev10 <ravitokas90@gmail.com>
2022-11-21 11:57:46 +0530

    add event listner to button

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 document.querySelector('button').addEventListener('click', getColor())
 
-let seedColor = document.querySelector('input').value
-let colorMode = document.querySelector('#color-mode').value
+
 
 function getColor(){
-fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${colorMode}`)
+    let seedColor = document.querySelector('input').value
+    let colorMode = document.querySelector('#color-mode').value
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${colorMode}`)
     .then(response => response.json())
     .then(data => console.log(data))
+
+    
 }
+
