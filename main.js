54e1ea0f4d47d4378b7386de2f1fc5163b2b8a0a
devmev10 <ravitokas90@gmail.com>
2022-11-21 12:17:20 +0530

    Add slice method to give proper hex value to API

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-document.querySelector('button').addEventListener('click', getColor())
+document.querySelector('button').addEventListener('click', getColor)
 
 
 
 function getColor(){
-    let seedColor = document.querySelector('input').value
+    let seedColor = document.querySelector('input').value.slice(1)
     let colorMode = document.querySelector('#color-mode').value
 
     fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${colorMode}`)
     .then(response => response.json())
     .then(data => console.log(data))
 
-    
+    console.log(seedColor)
+    console.log(colorMode)
 }
 
