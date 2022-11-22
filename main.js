f0b170b73303c8ad1122c0c8e465b2073e767f66
devmev10 <ravitokas90@gmail.com>
2022-11-22 15:13:23 +0530

    Troubleshoot API output

--- main.js
+++ main.js
@@ -6,16 +6,21 @@ function getColor(){
     let seedColor = document.querySelector('input').value.slice(1)
     let colorMode = document.querySelector('#color-mode').value
 
+    let colorScheme = []
     fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${colorMode}`)
     .then(response => response.json())
     .then(data => {
         // console.log(data.colors)
         let arr = data.colors
+
         for(item of arr){
-            console.log(item.hex.value)
+            // console.log(item.hex.value)
+            colorScheme.push(item.hex.value)
         }
     })
-
+    
+    console.log(colorScheme)
+    document.querySelector('#sec2').style.backgroundColor = `${colorScheme[1]}`
     
 }
 
